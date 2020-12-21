51b78876b81343054fbf13b1117acb26b883aeed
yuyatinnefeld <yuyatinnefeld@gmail.com>
2020-12-21 07:09:47 +0100

    add conf.py

---
+++ conf.py
@@ -0,0 +1,11 @@
+class WooCommerce():
+  def __init__(self):
+    self.cunsumer_key = 'ck_d30d8e9e39516a408e69a835c112f7121ce31d3e'
+    self.consumer_secret = 'cs_416994aa534b9fa7eebdd047ae58deb87eee4e93'
+
+
+  def get_key(self):
+    return self.cunsumer_key
+
+  def get_secret(self):
+    return self.consumer_secret
